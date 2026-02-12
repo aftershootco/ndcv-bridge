db7342452e47de6bc03f63ffdd3b124b28631a26
servius <servius@darksailor.dev>
2026-02-13 01:52:39 +0530

    feat: Added test cases for CvType

--- ndcv-bridge/src/types.rs
+++ ndcv-bridge/src/types.rs
@@ -1,13 +1,42 @@
 pub(crate) mod seal {
     pub trait Sealed {}
-    impl Sealed for u8 {}
-    impl Sealed for i8 {}
-    impl Sealed for i16 {}
-    impl Sealed for u16 {}
-    impl Sealed for i32 {}
-    impl Sealed for u32 {}
-    impl Sealed for f32 {}
-    impl Sealed for f64 {}
+    macro_rules! seal {
+        ($($t:ty),*) => {
+            $(
+                impl Sealed for $t {}
+            )*
+        };
+    }
+    seal!(
+        u8,
+        i8,
+        u16,
+        i16,
+        i32,
+        f32,
+        f64,
+        glam::Vec2,
+        glam::Vec3,
+        glam::Vec4,
+        glam::DVec2,
+        glam::DVec3,
+        glam::DVec4,
+        glam::U8Vec2,
+        glam::U8Vec3,
+        glam::U8Vec4,
+        glam::I8Vec2,
+        glam::I8Vec3,
+        glam::I8Vec4,
+        glam::U16Vec2,
+        glam::U16Vec3,
+        glam::U16Vec4,
+        glam::I16Vec2,
+        glam::I16Vec3,
+        glam::I16Vec4,
+        glam::IVec2,
+        glam::IVec3,
+        glam::IVec4
+    );
 }
 pub trait CvType: seal::Sealed + bytemuck::Pod {
     fn cv_depth() -> i32;
@@ -42,25 +71,31 @@ impl_cv_depth!(
     i32 => CV_32S,
     f32 => CV_32F,
     f64 => CV_64F,
-    glam::Vec2 => CV_32FC2 => 2,
-    glam::Vec3 => CV_32FC3 => 3,
-    glam::Vec4 => CV_32FC4 => 4,
-    glam::DVec2 => CV_64FC2 => 2,
-    glam::DVec3 => CV_64FC3 => 3,
-    glam::DVec4 => CV_64FC4 => 4,
-    glam::U8Vec2 => CV_8UC2 => 2,
-    glam::U8Vec3 => CV_8UC3 => 3,
-    glam::U8Vec4 => CV_8UC4 => 4,
-    glam::I8Vec2 => CV_8SC2 => 2,
-    glam::I8Vec3 => CV_8SC3 => 3,
-    glam::I8Vec4 => CV_8SC4 => 4,
-    glam::U16Vec2 => CV_16UC2 => 2,
-    glam::U16Vec3 => CV_16UC3 => 3,
-    glam::U16Vec4 => CV_16UC4 => 4,
-    glam::I16Vec2 => CV_16SC2 => 2,
-    glam::I16Vec3 => CV_16SC3 => 3,
-    glam::I16Vec4 => CV_16SC4 => 4,
-    glam::IVec2 => CV_32SC2 => 2,
-    glam::IVec3 => CV_32SC3 => 3,
-    glam::IVec4 => CV_32SC4 => 4
+    glam::Vec2 => CV_32F => 2,
+    glam::Vec3 => CV_32F => 3,
+    glam::Vec4 => CV_32F => 4,
+    glam::DVec2 => CV_64F => 2,
+    glam::DVec3 => CV_64F => 3,
+    glam::DVec4 => CV_64F => 4,
+    glam::U8Vec2 => CV_8U => 2,
+    glam::U8Vec3 => CV_8U => 3,
+    glam::U8Vec4 => CV_8U => 4,
+    glam::I8Vec2 => CV_8S => 2,
+    glam::I8Vec3 => CV_8S => 3,
+    glam::I8Vec4 => CV_8S => 4,
+    glam::U16Vec2 => CV_16U => 2,
+    glam::U16Vec3 => CV_16U => 3,
+    glam::U16Vec4 => CV_16U => 4,
+    glam::I16Vec2 => CV_16S => 2,
+    glam::I16Vec3 => CV_16S => 3,
+    glam::I16Vec4 => CV_16S => 4,
+    glam::IVec2 => CV_32S => 2,
+    glam::IVec3 => CV_32S => 3,
+    glam::IVec4 => CV_32S => 4
 );
+
+#[test]
+fn test_cv_type() {
+    assert_eq!(<u8 as CvType>::cv_type(), opencv::core::CV_8UC1);
+    assert_eq!(<glam::Vec3 as CvType>::cv_type(), opencv::core::CV_32FC3);
+}

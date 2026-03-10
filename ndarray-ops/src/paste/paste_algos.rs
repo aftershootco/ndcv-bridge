ac664e829a34deacfaec8daecab5e7ec8b43da01
Blanc <1mayanktomar@gmail.com>
2026-03-10 14:20:19 +0530

    fix: alpha not working as intended with subject mask

--- ndarray-ops/src/paste/paste_algos.rs
+++ ndarray-ops/src/paste/paste_algos.rs
@@ -2,7 +2,20 @@ use std::ops::{Add, Mul};
 
 use super::PasteInput;
 
-pub fn blend<T>(input: PasteInput<T>) -> T
+/// Blends based on subject mask (foreground = 1.)
+pub fn blend<T>(mut input: PasteInput<T>) -> T
+where
+    T: Copy,
+    T: Add<T, Output = T>,
+    T: Mul<T, Output = T>,
+    T: Mul<f32, Output = T>,
+{
+    input.mask = 1. - input.mask;
+    blend_inverted_mask(input)
+}
+
+/// Blends based on background mask (background = 1.)
+pub fn blend_inverted_mask<T>(input: PasteInput<T>) -> T
 where
     T: Copy,
     T: Add<T, Output = T>,
@@ -15,7 +28,7 @@ where
         mask,
         alpha,
     } = input;
-    let mask = mask * (1.0 - alpha);
+    let mask = mask * alpha;
 
-    this * mask + other * (1.0 - mask)
+    this * (1.0 - mask) + other * mask
 }
